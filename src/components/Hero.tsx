9fafdd10b290b4d49c21287714a4ecfd5432142e
mocanuhoratiu-dev <mocanuhoratiu@gmail.com>
2026-05-04 00:13:34 +0300

    Fix: Eliminare query string din hero image pentru compatibilitate build

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -92,7 +92,7 @@ export default function Hero() {
         >
           <div className="relative z-10 w-full animate-float will-change-transform">
             <Image
-              src="/hero-storybook.png?v=2"
+              src="/hero-storybook.png"
               alt="Ilustrație Magică"
               width={800}
               height={600}
